e01a1e167e8dd0c4897bf281435407d151df3d4c
chengyufang <cnyvfang@outlook.com>
2025-02-13 23:08:53 +0800

    Update the code.

---
+++ trainS1_Backlit.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4  --use_env --master_port=4388 Reti-Diff/train.py -opt options/trainS1_Backlit.yml --launcher pytorch
